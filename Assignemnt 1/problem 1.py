d565ad7b406c35487ce97f8d7f60b33bdc229c4a
ZawadX <zawadmahmud21344@gmail.com>
2026-02-15 05:51:46 +0600

    Create even-odd checker in problem 1.py
    Add a script to check if a number is even or odd.

---
+++ Assignemnt 1/problem 1.py
@@ -0,0 +1,12 @@
+"""
+Check if the number is Even or odd
+"""
+
+# Take input from user                       
+num = int(input("Enter a number: "))
+
+# Check even or odd
+if num % 2 == 0 :   # if Remainder that mean its not even  and remainder is 0 = even  simple 
+    print("Even")
+else:
+    print("Odd")
